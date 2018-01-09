aaab09da719cb4f3f1b4a688eb571e63141645c4
Chris-Snelson <chris.snelson@celigo.com>
2018-01-09 09:40:05 -0800

    Sample PreMap hook that logs data coming in

---
+++ preMap/samplePreMapHook.js
@@ -0,0 +1,53 @@
+/**
+ * Called from integrator.io prior to mapping the data
+ * @param  {[type]} options The data sent to the hook.  See SampleOptionsData.json for format
+ * @return [Array]         Array containing the data sent to the mapper
+ */
+var samplePreMapHook = function(options){
+	
+	//The array that will be returned from this hook to be processed by the mappings
+	var response = [];
+
+	for (var i = 0; i < options.data.length; i++) {
+		/* The response object contains of a data array and an error array
+		The data array is the elements that will be passed on to the mappings
+		The errors array will show up as failed records on the integrator.io dashboard.  
+		Each element in the array may consist of one or more errors
+		*/
+		
+		response.push({
+			data : JSON.parse(JSON.stringify(options.data[i])),
+			errors : []
+		});
+	}
+
+	try {
+		logOptions(options, response);
+	} catch (e) {
+		nlapi.logExecution('ERROR', e.name, e.message);
+		//In the case of a high level failure all records should be marked as failed
+		//If there is a single record failure the individual error should be logged in the function called
+		//within the try-catch block
+		for (var i = 0; i < response.length; i++) {
+			response[i].data = null;
+			response[i].errors.push({
+				code : e.name,
+				message : e.message
+			});
+		}
+	}
+
+	//Send the data to the mappings
+	return response;
+
+};
+
+/**
+ * Log the data passed into the hook into the SuiteScript logs of the RESTlet
+ * @param  {[type]} options  Data passed into the PreMap hook
+ * @param  [type] response The object that is passed on to the mappings
+ * @return null
+ */
+var logOptions = function(options, response){
+	nlapiLogExecution('AUDIT', 'Options', JSON.stringify(options));
+};
